53d400d91d293d1b26424f344f994f7f54c640b2
danila_s_i <danila.sergeevich.ivanov@gmail.com>
2021-10-01 21:17:32 +0300

    Исправлены отступы и убрано объявление временной переменной в функции getRandomIntegerFromRange

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 function getRandomIntegerFromRange(from, to) {
-    if (from > to) {
-        let temp = from;
-        from = to;
-        to = temp;
-    } else if (from == to) return from;
+  if (from > to) {
+    from -= to;
+    to += from;
+    from = to - from;
+  } else if (from === to) {return from;}
 
-    return from + Math.trunc((to - from + 1) * Math.random());
+  return from + Math.trunc((to - from + 1) * Math.random());
 }
 
-function isNotLonger (stringForCheck, maxLength) {
-    if (stringForCheck.length <= maxLength) return true;
+function isNotLonger(stringForCheck, maxLength) {
+  if (stringForCheck.length <= maxLength) {return true;}
 
-    return false;
+  return false;
 }
